001f3f5e6722af9404bade0eef9b32ea3b374878
Marsseo <ghktjddl3@naver.com>
2017-04-24 14:08:10 +0900

    연습파일 삭제

--- ClassA.java
+++
@@ -1,7 +0,0 @@
-public class ClassA{
-  public static void main(String[] args){
-
-    System.out.println("111");
-    System.out.pritln("222");
-  }
-}
--- ClassB.java
+++
@@ -1,5 +0,0 @@
-public class ClassA{
-  public static void main(String[] args){
-    System.out.println("User2가 작성");
-  }
-}
