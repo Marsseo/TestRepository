31e121d6baefab816803b50710c8f8604d097c21
Marsseo <ghktjddl3@naver.com>
2017-04-24 12:38:56 +0900

    수정

--- ClassA.java
+++ ClassA.java
@@ -1,8 +1,5 @@
 public class ClassA{
   public static void main(String[] args){
-    Sytem.out.println("User1이 작성");
-    Sytem.out.println("User2이 작성");
-    Sytem.out.println("User2이 작성2");
-    Sytem.out.println("User2이 작성3");
+    
   }
 }
