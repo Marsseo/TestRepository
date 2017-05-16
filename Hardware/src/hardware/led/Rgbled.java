6282f26dbf688a5c285c9c58f116446a8e639097
Marsseo <ghktjddl3@naver.com>
2017-05-16 18:33:40 +0900

    ㅇㅇㅇ

--- Hardware/src/hardware/led/Rgbled.java
+++ Hardware/src/hardware/led/Rgbled.java
@@ -8,49 +8,55 @@ import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
 
-public class DualColorLed {
-	
-	//Field
+public class Rgbled {
 	private GpioPinDigitalOutput redPin;
 	private GpioPinDigitalOutput greenPin;
+	private GpioPinDigitalOutput bluePin;
 	
 	//Constructor
-	public DualColorLed(Pin redPinNo, Pin greenPinNo){
+	public Rgbled(Pin redPinNo, Pin greenPinNo, Pin bluePinNo){
 		//싱글톤인 GpioController 생성하여 핀 세팅
 		GpioController gpioController = GpioFactory.getInstance();
-		redPin = gpioController.provisionDigitalOutputPin(redPinNo);
+		redPin = gpioController.provisionDigitalOutputPin(redPinNo, PinState.HIGH);
 		//어플리케이션이 종료될 때 출력 모드 해제하고 핀 출력을 LOW
 		redPin.setShutdownOptions(true, PinState.LOW);
+
 		
-		
-		greenPin = gpioController.provisionDigitalOutputPin(greenPinNo);
+		greenPin = gpioController.provisionDigitalOutputPin(greenPinNo, PinState.HIGH);
 		greenPin.setShutdownOptions(true, PinState.LOW);
+		
+		bluePin = gpioController.provisionDigitalOutputPin(bluePinNo, PinState.HIGH);
+		bluePin.setShutdownOptions(true, PinState.LOW);sss
 		
 	}
 	//Method
 	public void red(){
-		redPin.high();
-		greenPin.low();
+		redPin.low();
+		greenPin.high();
+		bluePin.high();
 	}
 	public void green(){
+		redPin.high();
+		greenPin.low();
+		bluePin.high();		
+	}
+	public void blue(){
+		redPin.high();
 		greenPin.high();
-		redPin.low();
+		bluePin.low();
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
 		
 		
-		DualColorLed test = new DualColorLed(RaspiPin.GPIO_21, RaspiPin.GPIO_22);
+		Rgbled test = new Rgbled(RaspiPin.GPIO_00, RaspiPin.GPIO_01, RaspiPin.GPIO_02);
 		while(true){
-			for(int i =0;i<3;i++){
-				test.green();
-			}
-			Thread.sleep(3000);
-			for(int i =0;i<3;i++){
-				test.red();
-			}
-			Thread.sleep(1000);
-			
+			test.red();
+			Thread.sleep(50);
+			test.green();
+			Thread.sleep(50);
+			test.blue();
+			Thread.sleep(50);
 		}
 	}
 }
